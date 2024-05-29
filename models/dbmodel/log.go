0b8b2ac13a1ea6473a26bb6fa33471c3689bd195
Markus <markus.schild.04@gmail.com>
2024-05-29 20:27:53 +0200

    encrypt database logs

--- models/dbmodel/log.go
+++ models/dbmodel/log.go
@@ -1,10 +1,60 @@
 package dbmodel
 
-import "time"
+import (
+	"mpt_data/helper"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Log struct {
 	LogLevel  uint
 	Source    string
 	Text      string
 	TimeStamp time.Time
+}
+
+// BeforeCreate hook for gorm
+func (l *Log) BeforeCreate(_ *gorm.DB) (err error) {
+	source, err := helper.EncryptDataToBase64(l.Source)
+	if err != nil {
+		return err
+	}
+	l.Source = source
+	text, err := helper.EncryptDataToBase64(l.Text)
+	if err != nil {
+		return err
+	}
+	l.Text = text
+	return
+}
+
+// AfterCreate hook for gorm
+func (l *Log) AfterCreate(_ *gorm.DB) (err error) {
+	source, err := helper.DecryptDataFromBase64(l.Source)
+	if err != nil {
+		return err
+	}
+	l.Source = string(source)
+	text, err := helper.DecryptDataFromBase64(l.Text)
+	if err != nil {
+		return err
+	}
+	l.Text = string(text)
+	return
+}
+
+// AfterFind hook for gorm
+func (l *Log) AfterFind(_ *gorm.DB) (err error) {
+	source, err := helper.DecryptDataFromBase64(l.Source)
+	if err != nil {
+		return err
+	}
+	l.Source = string(source)
+	text, err := helper.DecryptDataFromBase64(l.Text)
+	if err != nil {
+		return err
+	}
+	l.Text = string(text)
+	return
 }
